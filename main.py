9cca878759cbe4eb8308c28c488ba3ba25739fcb
soulking990 <61153665+soulking990@users.noreply.github.com>
2021-08-14 22:06:00 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,43 +2,35 @@ from logging import DEBUG, debug
 from typing import Text
 from flask.templating import render_template
 import requests
-import bs4
+import re
+from matplotlib import pyplot as plt
+import seaborn as sns
+
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
-def covid():
-    global text,numbers
-    text = []
-    numbers = []
-    url = 'https://www.worldometers.info/coronavirus/country/bangladesh/'
-    covid_data = requests.get(url)
-    soup = bs4.BeautifulSoup(covid_data.text,"lxml")
-    content = soup.find("div",class_ = "content-inner").find_all("div", id = "maincounter-wrap")
-    for x in content:
-        if "<h1>" in str(x):
-            iter_text = text.append(x.find("h1", class_ = None).get_text())
-            iter_numbers = numbers.append(x.find("span",class_=None).get_text())
+covid = requests.get('https://api.covid19api.com/live/country/bangladesh')
+data = covid.json()
+week_data = data[-7::1]
 
-covid()
+def deaths(data):
+    result = []
+    for day in data:
+        result.append(day['Deaths'])
+    return result
+
+x_axis =deaths(week_data)
+
+pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
+y_axis = re.findall(pattern, str(week_data))
 
 @app.route("/")
 def hello_world():
-    return "<p>Hey there! want to look at covid data? Try going to /total or /recovered even /deaths is available</p>"
+    return render_template('layout.html', x = x_axis, y = y_axis)
 
-@app.route("/total")
-def total():
-    return render_template('total.html',text = text,numbers = numbers)
-
-@app.route("/deaths")
-def deaths():
-    return render_template('deaths.html',text = text,numbers = numbers)
-
-@app.route("/recovered")
-def recovered():
-    return render_template('recovered.html',text = text,numbers = numbers)
 
 
 if __name__ == "__main__":
